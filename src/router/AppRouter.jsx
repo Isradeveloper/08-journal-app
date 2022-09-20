04e8731fa6806d747d510ad12dc952ffedf924b7
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-20 09:35:24 -0500

    Inicializando

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+
+      {/* Login y registro */}
+      <Route />
+
+      {/* Journal App */}
+      <Route />
+
+    </Routes>
+  )
+}
