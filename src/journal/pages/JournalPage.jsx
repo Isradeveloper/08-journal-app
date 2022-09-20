c0c6a2811a2537523cb1c3ecc4c17e261c28790f
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-20 15:14:52 -0500

    Nota vacia

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
-import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur eveniet nostrum nemo. Reprehenderit dolores iusto recusandae voluptate repellendus! Soluta dicta nisi enim dolor ullam ab quae alias dignissimos odio.</Typography>
+        {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur eveniet nostrum nemo. Reprehenderit dolores iusto recusandae voluptate repellendus! Soluta dicta nisi enim dolor ullam ab quae alias dignissimos odio.</Typography> */}
+        <NothingSelectedView />
       </JournalLayout>
     </>
   )
